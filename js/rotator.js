9d352c100381ec583472031e919bb5566f479548
Eva Moshkovich <evamoshkovich92@gmail.com>
2014-01-24 23:26:02 -0500

    Added movement capability, controlled by left/right/up/down keys

--- js/rotator.js
+++ js/rotator.js
@@ -1,7 +1,8 @@
 window.onload = function() {
 
-  var game = new Phaser.Game(600, 600, Phaser.AUTO, '', { preload: preload, create: create });
-
+  var game = new Phaser.Game(600, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+  var logo;
+  var keys;
   function preload () {
 
     game.load.image('logo', 'placeholder.png');
@@ -10,8 +11,24 @@ window.onload = function() {
 
   function create () {
 
-    var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
+    logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
     logo.anchor.setTo(0.5, 0.5);
+    keys = game.input.keyboard.createCursorKeys();
   }
-
+function update () {
+logo.body.velocity.setTo(0, 0);
+    if (keys.right.isDown){
+    logo.body.velocity.x = 200;
+	}
+	else if (keys.left.isDown){
+	 logo.body.velocity.x = -200;
+	 }
+	 else if( keys.up.isDown){
+	 logo.body.velocity.y = -200;
+	 }
+	 else if ( keys.down.isDown){
+	 logo.body.velocity.y = 200;
+	 }
+		
+	}
 };
