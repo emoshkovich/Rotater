4dadc9787fb21c4ff8a22efbe3eaff39edb3e0e6
Eva Moshkovich <evamoshkovich92@gmail.com>
2014-01-25 15:03:54 -0500

    Almost finished! (pairing with Kasif)

--- js/rotator.js
+++ js/rotator.js
@@ -1,12 +1,12 @@
 window.onload = function() {
 
-  var game = new Phaser.Game(600, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+  var game = new Phaser.Game(640, 640, Phaser.AUTO, '', { preload: preload, create: create, update: update });
   var player;
   var keys;
   var json = { "ps1": "img/Ps1.png", "ps2": "img/Ps2.png", "ps3": "img/Ps3.png", "ps4": "img/Ps4.png", "ps5": "img/Ps1.png", "ps6": "img/Ps2.png", "ps7": "img/Ps3.png", "ps8": "img/Ps4.png", "ps9": "img/Ps1.png", "ps10": "img/Ps2.png", "ps11": "img/Ps3.png", "ps12": "img/Ps4.png", "ps13": "img/Ps1.png", "ps14": "img/Ps2.png", "ps15": "img/Ps3.png", "ps16": "img/Ps4.png", "ps17": "img/Ps1.png", "ps18": "img/Ps2.png", "ps19": "img/Ps3.png", "ps20": "img/Ps3.png", "ps21": "img/Ps1.png", "ps22": "img/Ps2.png", "ps23": "img/Ps3.png", "ps24": "img/Ps4.png", "ps25": "img/Ps1.png", "ps26": "img/Ps2.png", "ps27": "img/Ps3.png", "ps28": "img/Ps4.png", "ps29": "img/Ps1.png", "ps30": "img/Ps2.png", "ps31": "img/Ps3.png", "ps32": "img/Ps4.png", "ps33": "img/Ps1.png", "ps34": "img/Ps2.png", "ps35": "img/Ps3.png", "ps36": "img/Ps4.png" };
   var tiles = new Array(6);
   var henry;
-  var henryIdle;
+  var startScreen;
   for (var i = 0; i<6; i++){
   	tiles[i] = new Array(6);
   }
@@ -15,24 +15,49 @@ window.onload = function() {
   
   function preload () {
     loadImages(function() {
+      game.load.image('bg', 'img/background.png');
       game.load.image('player', 'img/Phero.png');
       game.load.spritesheet('henrywalk', 'img/henrywalk.png', 40, 40, 18);
+      game.load.image('startScreen', 'img/introscreen.png');
+      game.load.image('endScreen', 'img/endscreen.png');
       keys = game.input.keyboard.createCursorKeys();
       jump = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
     });
   }
 
   function create () {
+    game.add.tileSprite(0, 0, 640, 640, 'bg');
     createImages(function() {
-      player = game.add.sprite(5, 5, 'henrywalk');
+      startScreen = game.add.tileSprite(0, 0, 640, 640, 'startScreen');
+      endScreen = game.add.tileSprite(0, 0, 640, 640, 'endScreen');
+      endScreen.visible = false;
+      player = game.add.sprite(70, 70, 'henrywalk');
       player.anchor.setTo(.5, .5);
       player.body.setSize(100, 100, 0, 0);
       player.animations.add('walk');
+      player.visible = false;
+      game.input.onDown.add(startGame, this);
     });
+  }
+  
+  function startGame() {
+  	startScreen.visible = false;
+  	player.visible = true;
+  }
+  
+  function endGame() {
+    player.kill();
+    endScreen.visible = true;
   }
 
   function update() {
   	var d = new Date();
+  	
+  	var x = Math.floor((player.x+70)/100);
+  	var y = Math.floor((player.y+70)/100);
+  	if (x === 5 && y === 4){
+  	  endGame();
+  	}
 	if (jump.isDown && canMove){
 		canMove = false;
 	  	rotate(player.x, player.y);
@@ -80,7 +105,7 @@ window.onload = function() {
     var i =0;
     var j=0;
     $.each(json, function(key, value) {
-      var temp = game.add.sprite(x+50, y+50, key);
+      var temp = game.add.sprite(x+70, y+70, key);
       temp.anchor.setTo(0.5,0.5);
       var obj;
       if(value.indexOf("1") !== -1){
