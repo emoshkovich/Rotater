669af895fa552d417dbf44fe30c851685e58dab0
Kasif Gilbert <dklg05@gmail.com>
2014-01-25 01:28:41 -0500

    I think I fixed it...

--- js/rotator.js
+++ js/rotator.js
@@ -3,6 +3,7 @@ window.onload = function() {
   var game = new Phaser.Game(600, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
   var player;
   var keys;
+  var json = { "ps1": "img/Ps1.png", "ps2": "img/Ps2.png", "ps3": "img/Ps3.png", "ps4": "img/Ps4.png", "ps5": "img/Ps1.png", "ps6": "img/Ps2.png", "ps7": "img/Ps3.png", "ps8": "img/Ps4.png", "ps9": "img/Ps1.png", "ps10": "img/Ps2.png", "ps11": "img/Ps3.png", "ps12": "img/Ps4.png", "ps13": "img/Ps1.png", "ps14": "img/Ps2.png", "ps15": "img/Ps3.png", "ps16": "img/Ps4.png", "ps17": "img/Ps1.png", "ps18": "img/Ps2.png", "ps19": "img/Ps3.png", "ps20": "img/Ps4.png", "ps21": "img/Ps1.png", "ps22": "img/Ps2.png", "ps23": "img/Ps3.png", "ps24": "img/Ps4.png", "ps25": "img/Ps1.png", "ps26": "img/Ps2.png", "ps27": "img/Ps3.png", "ps28": "img/Ps4.png", "ps29": "img/Ps1.png", "ps30": "img/Ps2.png", "ps31": "img/Ps3.png", "ps32": "img/Ps4.png", "ps33": "img/Ps1.png", "ps34": "img/Ps2.png", "ps35": "img/Ps3.png", "ps36": "img/Ps4.png" };
 
   function preload () {
     loadImages(function() {
@@ -35,27 +36,23 @@ window.onload = function() {
   }
 
   function loadImages(callback) {
-    $.getJSON('data/tiles.json', function(data) {
-      $.each(data, function(key, value) {
-        game.load.image(key, value);
-      });
+    $.each(json, function(key, value) {
+      game.load.image(key, value);
     });
     callback();
   }
 
   function createImages(callback) {
-    $.getJSON('data/tiles.json', function(data) {
-      var x = 0;
-      var y = 0;
-      $.each(data, function(key, value) {
-        game.add.sprite(x, y, key);
-        x += 100;
-        if (x % 600 === 0) {
-          y +=100;
-          x = 0;
-        }
-      });
+    var x = 0;
+    var y = 0;
+    $.each(json, function(key, value) {
+      game.add.sprite(x, y, key);
+      x += 100;
+      if (x % 600 === 0) {
+        y +=100;
+        x = 0;
+      }
     });
     callback();
   }
-};
+}
