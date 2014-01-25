a0272106696a98994fd10a34fde9184a7f49088b
Kasif Gilbert <dklg05@gmail.com>
2014-01-25 00:20:25 -0500

    Added images to map

--- js/rotator.js
+++ js/rotator.js
@@ -1,34 +1,57 @@
 window.onload = function() {
 
   var game = new Phaser.Game(600, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
-  var logo;
+  var player;
   var keys;
+
   function preload () {
-
-    game.load.image('logo', 'placeholder.png');
-
+    loadImages();
+    game.load.image('player', 'img/Phero.png');
+    keys = game.input.keyboard.createCursorKeys();
   }
 
   function create () {
-
-    logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
-    logo.anchor.setTo(0.5, 0.5);
-    keys = game.input.keyboard.createCursorKeys();
+    createImages();
+    player = game.add.sprite(5, 5, 'player');
+    player.bringToTop();
   }
-function update () {
-logo.body.velocity.setTo(0, 0);
+
+  function update() {
+    player.body.velocity.setTo(0, 0);
     if (keys.right.isDown){
-    logo.body.velocity.x = 200;
-	}
-	else if (keys.left.isDown){
-	 logo.body.velocity.x = -200;
-	 }
-	 else if( keys.up.isDown){
-	 logo.body.velocity.y = -200;
-	 }
-	 else if ( keys.down.isDown){
-	 logo.body.velocity.y = 200;
-	 }
-		
-	}
+      player.body.velocity.x = 200;
+    }
+    else if (keys.left.isDown) {
+      player.body.velocity.x = -200;
+    }
+    else if( keys.up.isDown) {
+      player.body.velocity.y = -200;
+    }
+    else if ( keys.down.isDown) {
+      player.body.velocity.y = 200;
+    }
+  }
+
+  function loadImages() {
+    $.getJSON('data/tiles.json', function(data) {
+      $.each(data, function(key, value) {
+        game.load.image(key, value);
+      });
+    });
+  }
+
+  function createImages() {
+    $.getJSON('data/tiles.json', function(data) {
+      var x = 0;
+      var y = 0;
+      $.each(data, function(key, value) {
+        game.add.sprite(x, y, key);
+        x += 100;
+        if (x % 600 === 0) {
+          y +=100;
+          x = 0;
+        }
+      });
+    });
+  }
 };
